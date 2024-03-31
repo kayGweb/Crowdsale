e29cfb01961a9723c4b2646ce27b55b548f1d1e4
Ideal Kendall <antonio.kendall@gmail.com>
2024-03-31 18:22:50 -0400

    Added Buy Tokens functionaility

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,6 +1,29 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract Crowdsale {
-    string public name = "Crowdsale";
+    Token public token;
+    uint256 public price;
+    uint256 public maxTokens;
+    uint256 public tokensSold;
+
+    event Buy(uint256 amount, address buyer);
+
+    constructor(Token _token, uint256 _price, uint256 _maxTokens) {
+        token = _token;
+        price = _price;
+        maxTokens = _maxTokens;
+    }
+
+    function buyTokens(uint256 _amount) public payable {
+        require(msg.value == (_amount / 1e18) * price, "Invalid Amount");
+        require(token.balanceOf(address(this)) >= _amount); //write test for his homework
+        require(token.transfer(msg.sender, _amount));
+        //Update token sold
+        tokensSold += _amount;
+        //emit event
+        emit Buy(_amount, msg.sender);
+    }
 }
--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -5,18 +5,83 @@ const tokens = (n) => {
 	return ethers.utils.parseEther(n.toString(), "ether");
 };
 
-describe("Crowdsale", function () {
-	let crowdsale;
+const ether = tokens;
 
-	beforeEach(async function () {
+describe("Crowdsale", () => {
+	let crowdsale, token, accounts, deployer, user1;
+
+	beforeEach(async () => {
+		//Load Contracts
 		const Crowdsale = await ethers.getContractFactory("Crowdsale");
-		crowdsale = await Crowdsale.deploy();
-		await crowdsale.deployed();
+		const Token = await ethers.getContractFactory("Token");
+
+		//Deploy Token
+		token = await Token.deploy("JayBird Token", "JBT", 1000000);
+
+		//Configure accounts
+		accounts = await ethers.getSigners();
+		deployer = accounts[0];
+		user1 = accounts[1];
+
+		//Deploy Crowdsale
+		crowdsale = await Crowdsale.deploy(token.address, ether(1), 1000000);
+
+		//Send tokens to crowdsale
+		let transaction = await token.connect(deployer).transfer(crowdsale.address, tokens(1000000));
+		await transaction.wait();
 	});
 
-	describe("Deployment", function () {
-		it("has correct name", async function () {
-			expect(await crowdsale.name()).to.equal("Crowdsale");
+	describe("Deployment", () => {
+		it("sends tokens ato the Crowdsale contract", async () => {
+			expect(await token.balanceOf(crowdsale.address)).to.be.equal(tokens(1000000));
+		});
+
+		it("returns the price", async () => {
+			expect(await crowdsale.price()).to.be.equal(ether(1));
+		});
+
+		it("returns token address", async () => {
+			expect(await crowdsale.token()).to.be.equal(token.address);
+		});
+	});
+
+	describe("Buying tokens", () => {
+		let amount = tokens(10);
+		let tranction, result;
+
+		describe("Success", () => {
+			beforeEach(async () => {
+				transaction = await crowdsale.connect(user1).buyTokens(amount, { value: ether(10) });
+				result = await transaction.wait();
+			});
+
+			it("transfers tokens", async () => {
+				expect(await token.balanceOf(user1.address)).to.be.eq(amount);
+				expect(await token.balanceOf(crowdsale.address)).to.be.eq(tokens(999990));
+			});
+
+			it("updates contracts ether balance", async () => {
+				expect(await ethers.provider.getBalance(crowdsale.address)).to.be.eq(amount);
+			});
+
+			it("updates tokensSold", async () => {
+				expect(await crowdsale.tokensSold()).to.eq(amount);
+			});
+
+			it("emits a Buy event", async () => {
+				// --> https://hardhat.org/hardhat-chai-matchers/docs/reference#.emit
+				await expect(transaction).to.emit(crowdsale, "Buy").withArgs(amount, user1.address);
+			});
+		});
+
+		describe("Failure", () => {
+			it("rejects insufficent ether", async () => {
+				await expect(crowdsale.connect(user1).buyTokens(amount, { value: 0 })).to.be.revertedWith("Invalid Amount");
+			});
+
+			//it("rejects insufficent tokens", async () => {
+			//await expect(crowdsale.connect(user1).buyTokens(tokens(1000001), {value: ether(10)})).to.be.revertedWith("Insufficient tokens in contract");
+			//});
 		});
 	});
 });
